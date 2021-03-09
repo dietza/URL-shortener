c4c89e4093fbcfc93edb2c1ce80b4eaa7d015a0e
Allison Dietz <dietz.allison@outlook.com>
2021-03-09 12:25:43 -0700

    Add Cypress test of posting new url

--- cypress/integration/urlShortener_spec.js
+++ cypress/integration/urlShortener_spec.js
@@ -1,3 +1,5 @@
+import mockPostBody from '../fixtures/mockPost.json'
+
 describe('URL Shortener', () => {
 
   const baseUrl = 'http://localhost:3000'
@@ -9,6 +11,15 @@ describe('URL Shortener', () => {
       cy.intercept('GET', 'http://localhost:3001/api/v1/urls', {
         statusCode: 200,
         body: mockUrlsData
+      })
+    })
+
+    cy
+    .fixture('mockPost.json')
+    .then((mockPostBody) => {
+      cy.intercept('POST', 'http://localhost:3001/api/v1/urls', {
+        statusCode: 200,
+        body: mockPostBody
       })
     })
     
@@ -41,11 +52,28 @@ describe('URL Shortener', () => {
       .find('.submit-btn').should('contain', 'Shorten Please')
   });
 
-  it ('Should update with the info a user enters into the fields', () => {
+  it.skip ('Should update with the info a user enters into the fields', () => {
     cy
       .get('form .title-input[type=text]').type('Title of New URL to add')
       .get('.urlToShorten-input[type=text]').type('https://http.cat/301')
       .get('form .submit-btn').click()
   });
+
+  it ('Should add a Url successfully submitted by the user', () => {
+    cy
+      .get('section')
+      .find('.url').should('have.length', 4)
+
+      .get('form .title-input[type=text]').type(`${mockPostBody.title}`)
+      .get('.urlToShorten-input[type=text]').type(`${mockPostBody.long_url}`)
+      .get('form .submit-btn').click()
+
+      .get('section')
+      .find('.url h3').contains('Stubbing')
+
+      .get('section')
+      .find('.url').should('have.length', 5)
+  });
+
 
 })
